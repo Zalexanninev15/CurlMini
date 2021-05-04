113d9773a4be827bb898159f6c4cb99f7b051531
Zalexanninev15 <blue.shark@disroot.org>
2021-05-04 15:56:01 +0300

    Improvements

--- CurlMini/Program.cs
+++ CurlMini/Program.cs
@@ -6,20 +6,11 @@ namespace CurlMini
 {
     static class Program
     {
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            using (RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Software"))
-            {
-                reg.CreateSubKey("Zalexanninev15");
-            }
-            using (RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Software\Zalexanninev15"))
-            {
-                reg.CreateSubKey("CurlMini");
-            }
+            using (RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Software")) { reg.CreateSubKey("Zalexanninev15"); }
+            using (RegistryKey reg = Registry.CurrentUser.CreateSubKey(@"Software\Zalexanninev15")) { reg.CreateSubKey("CurlMini"); }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
