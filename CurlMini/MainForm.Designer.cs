7b780f6d6d476943c7799c7a685d1be018645830
Zalexanninev15 <blue.shark@disroot.org>
2021-05-27 11:22:55 +0300

    Updated GitHub icon

--- CurlMini/MainForm.Designer.cs
+++ CurlMini/MainForm.Designer.cs
@@ -322,9 +322,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::CurlMini.Properties.Resources.GitHub;
-            this.pictureBox1.Location = new System.Drawing.Point(720, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(719, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(32, 34);
+            this.pictureBox1.Size = new System.Drawing.Size(37, 34);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
